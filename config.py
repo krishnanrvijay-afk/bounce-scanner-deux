1823df6e3fc3cef0a1fcba21a4319748fda7381c
krishnanrvijay-afk <krishnan.r.vijay@gmail.com>
2026-06-15 22:11:39 -0400

    config: revert to PAPER_MODE=True while fixing close precision bug

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ PAIRS = ["DOGE", "SUI", "BTC", "LINK", "ETH", "NEAR", "XRP", "SOL", "WIF", "AVAX
 
 SCAN_INTERVAL_SECONDS  = 30
 PRICE_INTERVAL_SECONDS = 8
-PAPER_MODE             = False
+PAPER_MODE             = True
 
 # -- Live trading safety --------------------------------------------------------
 # When PAPER_MODE is False and LIVE_MANUAL_ENTRY_ONLY is True, the scanner will
